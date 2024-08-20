6d937684d96587e4600ea3292be99f3d8241ce17
Thomas <thomas.fractalbits@gmail.com>
2024-08-19 23:47:37 -0700

    update proto related crates to make axum work with protobuf
    by enabling macro `axum::debug_handler` for RCA debugging

--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -1,22 +1,15 @@
 use api_server::{nss_get_inode, nss_ops, nss_put_inode};
 use axum::{routing::post, Router};
-// TODO: use axum_extra::protobuf::Protobuf;
+use axum_extra::protobuf::Protobuf;
 
-// async fn get_obj() -> Protobuf<nss_ops::GetInodeResponse> {
-async fn get_obj() -> String {
-    let resp = nss_get_inode("hello".into()).await.result;
-    if let Some(nss_ops::get_inode_response::Result::Ok(value)) = resp {
-        value
-    } else {
-        todo!()
-    }
+async fn get_obj() -> Protobuf<nss_ops::GetInodeResponse> {
+    nss_get_inode("hello".into()).await.into()
 }
 
-// async fn put_obj() -> Protobuf<nss_ops::PutInodeResponse> {
-async fn put_obj() -> () {
+async fn put_obj() -> Protobuf<nss_ops::PutInodeResponse> {
     let key: String = "hello".into();
     let value: String = key.chars().rev().collect();
-    _ = nss_put_inode(key, value).await;
+    nss_put_inode(key, value).await.into()
 }
 
 #[tokio::main]
