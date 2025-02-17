f8e1f223227c37db7e34d927a237cbe38a15d340
Thomas <thomas.fractalbits@gmail.com>
2025-02-17 10:51:43 -0800

    refactor(api_server): make bucket_tables to be a shared module
    Move rss rpc client out of bucket_tables module

--- api_server/src/bucket_tables.rs
+++ api_server/src/bucket_tables.rs
@@ -1,3 +1,23 @@
 pub mod api_key_table;
 pub mod bucket_table;
 pub mod table;
+
+use std::sync::Arc;
+
+use bytes::Bytes;
+use rpc_client_rss::*;
+use table::KvClient;
+
+impl KvClient for Arc<RpcClientRss> {
+    async fn put(&mut self, key: String, value: Bytes) -> Bytes {
+        RpcClientRss::put(self, key.into(), value).await.unwrap()
+    }
+
+    async fn get(&mut self, key: String) -> Bytes {
+        RpcClientRss::get(self, key.into()).await.unwrap()
+    }
+
+    async fn delete(&mut self, key: String) -> Bytes {
+        RpcClientRss::delete(self, key.into()).await.unwrap()
+    }
+}
--- api_server/src/bucket_tables/table.rs
+++ api_server/src/bucket_tables/table.rs
@@ -1,8 +1,7 @@
 #![allow(dead_code)]
 
 use bytes::Bytes;
-use rpc_client_rss::*;
-use std::{marker::PhantomData, sync::Arc};
+use std::marker::PhantomData;
 
 pub trait Entry: serde::Serialize {
     fn key(&self) -> String;
@@ -18,20 +17,6 @@ pub trait KvClient {
     async fn put(&mut self, key: String, value: Bytes) -> Bytes;
     async fn get(&mut self, key: String) -> Bytes;
     async fn delete(&mut self, key: String) -> Bytes;
-}
-
-impl KvClient for Arc<RpcClientRss> {
-    async fn put(&mut self, key: String, value: Bytes) -> Bytes {
-        RpcClientRss::put(self, key.into(), value).await.unwrap()
-    }
-
-    async fn get(&mut self, key: String) -> Bytes {
-        RpcClientRss::get(self, key.into()).await.unwrap()
-    }
-
-    async fn delete(&mut self, key: String) -> Bytes {
-        RpcClientRss::delete(self, key.into()).await.unwrap()
-    }
 }
 
 pub struct Table<C: KvClient, F: TableSchema> {
