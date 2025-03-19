482b65962495c03d2550ef725672802359f6d706
Thomas <thomas.fractalbits@gmail.com>
2025-03-19 15:19:46 -0700

    fix(api_server): try to filter empty xml output
    Tested with aws s3api:
    ```
    λ  playground git:(main) ✗ cat test_put_object.sh
    
    set -ex
    
    export AWS_DEFAULT_REGION=us-east-1
    export AWS_ENDPOINT_URL_S3='http://localhost:3000'
    export AWS_ACCESS_KEY_ID=test_api_key
    export AWS_SECRET_ACCESS_KEY=test_api_secret
    export AWS_PAGER=""
    
    bucket="test_bucket"
    key="my-dir/Cargo.toml"
    aws s3api create-bucket --bucket $bucket
    aws s3api put-object --bucket $bucket --key $key --body Cargo.toml --checksum-algorithm CRC32
    aws s3api get-object-attributes --bucket $bucket --key $key --object-attributes Etag,Checksum
    aws s3api delete-object --bucket test_bucket --key my-dir/Cargo.toml
    aws s3api delete-bucket --bucket $bucket
    
    λ  playground git:(main) ✗ ./test_put_object.sh
    + export AWS_DEFAULT_REGION=us-east-1
    + AWS_DEFAULT_REGION=us-east-1
    + export AWS_ENDPOINT_URL_S3=http://localhost:3000
    + AWS_ENDPOINT_URL_S3=http://localhost:3000
    + export AWS_ACCESS_KEY_ID=test_api_key
    + AWS_ACCESS_KEY_ID=test_api_key
    + export AWS_SECRET_ACCESS_KEY=test_api_secret
    + AWS_SECRET_ACCESS_KEY=test_api_secret
    + export AWS_PAGER=
    + AWS_PAGER=
    + bucket=test_bucket
    + key=my-dir/Cargo.toml
    + aws s3api create-bucket --bucket test_bucket
    {
        "Location": "/test_bucket"
    }
    + aws s3api put-object --bucket test_bucket --key my-dir/Cargo.toml --body Cargo.toml --checksum-algorithm CRC32
    + aws s3api get-object-attributes --bucket test_bucket --key my-dir/Cargo.toml --object-attributes Etag,Checksum
    {
        "LastModified": "2025-03-19T22:22:51+00:00",
        "ETag": "b909f3b99e9c521e0ffd9429c7f1c5ad",
        "Checksum": {
            "ChecksumCRC32": "Iv5jjg==",
            "ChecksumType": "CRC32"
        }
    }
    + aws s3api delete-object --bucket test_bucket --key my-dir/Cargo.toml
    + aws s3api delete-bucket --bucket test_bucket
    ```

--- api_server/src/handler/get/get_object_attributes.rs
+++ api_server/src/handler/get/get_object_attributes.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::handler::{
     common::{
         get_raw_object, response::xml::Xml, s3_error::S3Error, signature::checksum::ChecksumValue,
@@ -6,8 +8,10 @@ use crate::handler::{
     Request,
 };
 use axum::{
-    http::HeaderValue,
+    extract::Query,
+    http::{header::LAST_MODIFIED, HeaderValue},
     response::{IntoResponse, Response},
+    RequestPartsExt,
 };
 use base64::{prelude::BASE64_STANDARD, Engine};
 use bucket_tables::bucket_table::Bucket;
@@ -16,36 +20,100 @@ use serde::{Deserialize, Serialize};
 
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
-#[serde(rename_all = "kebab-case")]
-pub struct GetObjectAttributesOptions {
-    #[serde(rename(deserialize = "versionId"))]
+#[serde(rename_all = "camelCase")]
+struct QueryOpts {
     version_id: Option<String>,
-    response_cache_control: Option<String>,
-    response_content_disposition: Option<String>,
-    response_content_encoding: Option<String>,
-    response_content_language: Option<String>,
-    response_content_type: Option<String>,
-    response_expires: Option<String>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Default)]
+struct HeaderOpts {
+    x_amz_max_parts: Option<String>,
+    x_amz_part_number_marker: Option<String>,
+    x_amz_server_side_encryption_customer_algorithm: Option<String>,
+    x_amz_server_side_encryption_customer_key: Option<String>,
+    x_amz_server_side_encryption_customer_key_md5: Option<String>,
+    x_amz_request_payer: Option<String>,
+    x_amz_expected_bucket_owner: Option<String>,
+    x_amz_object_attributes: HashSet<String>,
+}
+
+impl HeaderOpts {
+    fn from_headers(headers: &axum::http::HeaderMap) -> Result<Self, S3Error> {
+        Ok(Self {
+            x_amz_max_parts: headers
+                .get("x-amz-max-parts")
+                .map(|x| x.to_str())
+                .transpose()?
+                .map(|x| x.to_owned()),
+            x_amz_part_number_marker: headers
+                .get("x-amz-part-number-marker")
+                .map(|x| x.to_str())
+                .transpose()?
+                .map(|x| x.to_owned()),
+            x_amz_server_side_encryption_customer_algorithm: headers
+                .get("x-amz-server-side-encryption-customer-algorithm")
+                .map(|x| x.to_str())
+                .transpose()?
+                .map(|x| x.to_owned()),
+            x_amz_server_side_encryption_customer_key: headers
+                .get("x-amz-server-side-encryption-customer-key")
+                .map(|x| x.to_str())
+                .transpose()?
+                .map(|x| x.to_owned()),
+            x_amz_server_side_encryption_customer_key_md5: headers
+                .get("x-amz-server-side-encryption-customer-key-MD5")
+                .map(|x| x.to_str())
+                .transpose()?
+                .map(|x| x.to_owned()),
+            x_amz_request_payer: headers
+                .get("x-amz-request-payer")
+                .map(|x| x.to_str())
+                .transpose()?
+                .map(|x| x.to_owned()),
+            x_amz_expected_bucket_owner: headers
+                .get("x-amz-expected-bucket-owner")
+                .map(|x| x.to_str())
+                .transpose()?
+                .map(|x| x.to_owned()),
+            x_amz_object_attributes: headers
+                .get("x-amz-object-attributes")
+                .ok_or(S3Error::InvalidArgument2)?
+                .to_str()?
+                .split(',')
+                .map(|x| x.to_lowercase())
+                .collect(),
+            ..Default::default()
+        })
+    }
 }
 
 #[derive(Default, Debug, Serialize, PartialEq, Eq)]
 #[serde(rename_all = "PascalCase")]
 struct GetObjectAttributesOutput {
-    etag: String,
-    checksum: CheckSum,
-    object_parts: ObjectParts,
-    storage_class: String,
-    object_size: usize,
+    #[serde(rename = "ETag", skip_serializing_if = "Option::is_none")]
+    etag: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    checksum: Option<Checksum>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    object_parts: Option<ObjectParts>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    storage_class: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    object_size: Option<usize>,
 }
 
 impl GetObjectAttributesOutput {
     fn etag(self, etag: String) -> Self {
-        Self { etag, ..self }
+        Self {
+            etag: Some(etag),
+            ..self
+        }
     }
 
     fn object_size(self, object_size: usize) -> Self {
         Self {
-            object_size,
+            object_size: Some(object_size),
             ..self
         }
     }
@@ -53,20 +121,36 @@ impl GetObjectAttributesOutput {
     fn checksum(mut self, checksum: Option<ChecksumValue>) -> Self {
         match checksum {
             Some(ChecksumValue::Crc32(crc32)) => {
-                self.checksum.checksum_crc32 = BASE64_STANDARD.encode(crc32);
-                self.checksum.checksum_type = "CRC32".to_string();
+                let checksum = Checksum {
+                    checksum_crc32: Some(BASE64_STANDARD.encode(crc32)),
+                    checksum_type: "CRC32".to_string(),
+                    ..Default::default()
+                };
+                self.checksum = Some(checksum);
             }
             Some(ChecksumValue::Crc32c(crc32c)) => {
-                self.checksum.checksum_crc32c = BASE64_STANDARD.encode(crc32c);
-                self.checksum.checksum_type = "CRC32C".to_string();
+                let checksum = Checksum {
+                    checksum_crc32c: Some(BASE64_STANDARD.encode(crc32c)),
+                    checksum_type: "CRC32C".to_string(),
+                    ..Default::default()
+                };
+                self.checksum = Some(checksum);
             }
             Some(ChecksumValue::Sha1(sha1)) => {
-                self.checksum.checksum_sha1 = BASE64_STANDARD.encode(sha1);
-                self.checksum.checksum_type = "SHA1".to_string();
+                let checksum = Checksum {
+                    checksum_sha1: Some(BASE64_STANDARD.encode(sha1)),
+                    checksum_type: "SHA1".to_string(),
+                    ..Default::default()
+                };
+                self.checksum = Some(checksum);
             }
             Some(ChecksumValue::Sha256(sha256)) => {
-                self.checksum.checksum_sha256 = BASE64_STANDARD.encode(sha256);
-                self.checksum.checksum_type = "SHA256".to_string();
+                let checksum = Checksum {
+                    checksum_sha256: Some(BASE64_STANDARD.encode(sha256)),
+                    checksum_type: "SHA256".to_string(),
+                    ..Default::default()
+                };
+                self.checksum = Some(checksum);
             }
             None => (),
         }
@@ -76,17 +160,17 @@ impl GetObjectAttributesOutput {
 
 #[derive(Default, Debug, Serialize, PartialEq, Eq)]
 #[serde(rename_all = "PascalCase")]
-struct CheckSum {
-    #[serde(rename = "ChecksumCRC32")]
-    checksum_crc32: String,
-    #[serde(rename = "ChecksumCRC32C")]
-    checksum_crc32c: String,
-    #[serde(rename = "ChecksumCRC64NVME")]
-    checksum_crc64nvme: String,
-    #[serde(rename = "ChecksumSHA1")]
-    checksum_sha1: String,
-    #[serde(rename = "ChecksumSHA256")]
-    checksum_sha256: String,
+struct Checksum {
+    #[serde(rename = "ChecksumCRC32", skip_serializing_if = "Option::is_none")]
+    checksum_crc32: Option<String>,
+    #[serde(rename = "ChecksumCRC32C", skip_serializing_if = "Option::is_none")]
+    checksum_crc32c: Option<String>,
+    #[serde(rename = "ChecksumCRC64NVME", skip_serializing_if = "Option::is_none")]
+    checksum_crc64nvme: Option<String>,
+    #[serde(rename = "ChecksumSHA1", skip_serializing_if = "Option::is_none")]
+    checksum_sha1: Option<String>,
+    #[serde(rename = "ChecksumSHA256", skip_serializing_if = "Option::is_none")]
+    checksum_sha256: Option<String>,
     checksum_type: String,
 }
 
@@ -112,37 +196,30 @@ struct Part {
     size: usize,
 }
 
-#[allow(dead_code)]
-#[derive(Default, Debug, Serialize, PartialEq, Eq)]
-#[serde(rename_all = "kebab-case")]
-struct ResponseHeaders {
-    #[serde(rename = "Last-Modified")]
-    last_modified: Option<String>, // timestamp
-    x_amz_delete_marker: Option<String>,
-    x_amz_request_charged: Option<String>,
-    x_amz_version_id: Option<String>,
-}
-
 pub async fn get_object_attributes(
-    _request: Request,
+    request: Request,
     bucket: &Bucket,
     key: String,
     rpc_client_nss: &RpcClientNss,
 ) -> Result<Response, S3Error> {
+    let header_opts = HeaderOpts::from_headers(request.headers())?;
+    let Query(_query_opts): Query<QueryOpts> = request.into_parts().0.extract().await?;
     let obj = get_raw_object(rpc_client_nss, bucket.root_blob_name.clone(), key.clone()).await?;
-    let etag = obj.etag()?;
-    let object_size = obj.size()? as usize;
-    let checksum = obj.checksum()?;
     let last_modified = time::format_http_date(obj.timestamp);
 
-    let mut resp = Xml(GetObjectAttributesOutput::default()
-        .etag(etag)
-        .object_size(object_size)
-        .checksum(checksum))
-    .into_response();
-    resp.headers_mut().insert(
-        "Last-Modified",
-        HeaderValue::from_str(&last_modified).unwrap(),
-    );
+    let mut resp = GetObjectAttributesOutput::default();
+    if header_opts.x_amz_object_attributes.contains("etag") {
+        resp = resp.etag(obj.etag()?);
+    }
+    if header_opts.x_amz_object_attributes.contains("checksum") {
+        resp = resp.checksum(obj.checksum()?);
+    }
+    if header_opts.x_amz_object_attributes.contains("objectsize") {
+        resp = resp.object_size(obj.size()? as usize);
+    }
+    // TODO: ObjectParts | StorageClass
+    let mut resp = Xml(resp).into_response();
+    resp.headers_mut()
+        .insert(LAST_MODIFIED, HeaderValue::from_str(&last_modified)?);
     Ok(resp)
 }
