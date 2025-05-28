5caeb416ad2dec3e2f1875a695135f6565eb0ba9
Thomas <thomas.fractalbits@gmail.com>
2025-05-27 20:35:39 -0700

    deploy: add ssm support
    e.g with ssm connection, we can access servers with private ips:
    ```
    sh-5.2$ sudo -i -u ec2-user
    [ec2-user@ip-10-0-1-24 ~]$ ls
    [ec2-user@ip-10-0-1-24 ~]$ aws s3 ls
    2025-05-25 18:49:34 cdk-hnb659fds-assets-ACCOUNT_ID_OWNER-us-west-1
    2025-05-22 22:57:24 do-not-delete-ssm-diagnosis-ACCOUNT_ID_OWNER-us-west-1-uvg9h
    2025-05-12 21:30:03 fractalbits-bucket
    ```

--- cdk/fractalbits-vpc/lib/fractalbits-vpc-stack.ts
+++ cdk/fractalbits-vpc/lib/fractalbits-vpc-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class FractalbitsVpcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -25,22 +26,51 @@ export class FractalbitsVpcStack extends cdk.Stack {
       ],
     });
 
-    // Add S3 Gateway VPC endpoint
+    // Add S3 VPC endpoint
     vpc.addGatewayEndpoint('S3Endpoint', {
       service: ec2.GatewayVpcEndpointAwsService.S3,
-      subnets: [{ subnetType: ec2.SubnetType.PRIVATE_ISOLATED }],
+      subnets: [
+        { subnetType: ec2.SubnetType.PUBLIC },
+        { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+      ],
+    });
+
+    // Add SSM VPC Endpoints (required for private subnet)
+    vpc.addInterfaceEndpoint('SSMEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.SSM,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    vpc.addInterfaceEndpoint('SSMMessagesEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    vpc.addInterfaceEndpoint('EC2MessagesEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.EC2_MESSAGES,
+      subnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    // IAM role for EC2 with SSM + S3
+    const ec2Role = new iam.Role(this, 'InstanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+    ec2Role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+    );
+    ec2Role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess')
+    );
+
+    // Security Group (no inbound rules; all outbound allowed)
+    const sg = new ec2.SecurityGroup(this, 'InstanceSG', {
+      vpc,
+      description: 'Allow outbound only for SSM and S3 access',
+      allowAllOutbound: true,
     });
 
     // Amazon Linux 2023 AMI
     const ami = ec2.MachineImage.latestAmazonLinux2023();
-
-    // Security group for all instances
-    const sg = new ec2.SecurityGroup(this, 'InstanceSG', {
-      vpc,
-      description: 'Allow SSH access',
-      allowAllOutbound: true,
-    });
-    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'Allow SSH');
 
     const apiServerInstance = new ec2.Instance(this, 'api_server', {
       vpc,
@@ -48,7 +78,7 @@ export class FractalbitsVpcStack extends cdk.Stack {
       machineImage: ami,
       vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
       securityGroup: sg,
-      keyName: 'api_server',
+      role: ec2Role,
     });
 
     new ec2.Instance(this, 'root_server', {
@@ -57,7 +87,7 @@ export class FractalbitsVpcStack extends cdk.Stack {
       machineImage: ami,
       vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
       securityGroup: sg,
-      keyName: 'root_server',
+      role: ec2Role,
     });
 
     new ec2.Instance(this, 'nss_server', {
@@ -66,7 +96,7 @@ export class FractalbitsVpcStack extends cdk.Stack {
       machineImage: ami,
       vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
       securityGroup: sg,
-      keyName: 'nss_server',
+      role: ec2Role,
     });
 
     new ec2.Instance(this, 'bss_server', {
@@ -75,7 +105,7 @@ export class FractalbitsVpcStack extends cdk.Stack {
       machineImage: ami,
       vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
       securityGroup: sg,
-      keyName: 'bss_server',
+      role: ec2Role,
     });
 
     // Outputs
