c0bb816dd3bc61bf362e08d6460a3fb3ca843c20
zhu <zzsheng@gmail.com>
2025-06-07 00:39:36 +0800

    add rename test in precheckin

--- xtask/src/cmd_precheckin.rs
+++ xtask/src/cmd_precheckin.rs
@@ -40,6 +40,18 @@ pub fn run_cmd_precheckin() -> CmdResult {
         e
     })?;
 
+    let async_art_log = "test_async_art_rename.log";
+    run_cmd! {
+        info "Running async art rename tests with log $async_art_log ...";
+        ./zig-out/bin/mkfs;
+        ./zig-out/bin/fbs --new_tree $TEST_BUCKET_ROOT_BLOB_NAME;
+        ./zig-out/bin/test_async_art --prefill 100000 --tests rename --ops 10000 --parallelism 1000 --debug  &> $async_art_log; 
+    }
+    .map_err(|e| {
+        run_cmd!(tail $async_art_log).unwrap();
+        e
+    })?;
+
     let async_art_log = "test_async_art.log";
     run_cmd! {
         info "Running async art tests with log $async_art_log ...";
