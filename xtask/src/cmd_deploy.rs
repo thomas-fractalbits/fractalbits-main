dda75fef0cca16200808580f7ad6dd97e2e824cf
Thomas <thomas.fractalbits@gmail.com>
2025-06-09 20:14:05 -0700

    deploy: allow other account to run xtask deploy
    As long as it has putObject access permission
    (e.g TARGET_EMAIL@example.com)

--- xtask/src/cmd_deploy.rs
+++ xtask/src/cmd_deploy.rs
@@ -7,15 +7,6 @@ pub fn run_cmd_deploy(use_s3_backend: bool) -> CmdResult {
     let bucket_name = get_build_bucket_name()?;
     let bucket = format!("s3://{bucket_name}");
 
-    // Check if the bucket exists; create if it doesn't
-    let bucket_exists = run_cmd!(aws s3api head-bucket --bucket $bucket_name &>/dev/null).is_ok();
-    if !bucket_exists {
-        run_cmd! {
-            info "Creating bucket $bucket";
-            aws s3 mb $bucket
-        }?;
-    }
-
     const BUILD_MODE: &str = "release";
     run_cmd! {
         info "Building with zigbuild";
@@ -69,6 +60,15 @@ pub fn update_builds_bucket_access_policy() -> CmdResult {
     }
 
     let bucket_name = get_build_bucket_name()?;
+    let bucket = format!("s3://{bucket_name}");
+    // Check if the bucket exists; create if it doesn't
+    let bucket_exists = run_cmd!(aws s3api head-bucket --bucket $bucket_name &>/dev/null).is_ok();
+    if !bucket_exists {
+        run_cmd! {
+            info "Creating bucket $bucket";
+            aws s3 mb $bucket
+        }?;
+    }
 
     // Remove Block Public Access
     let new_public_access_conf = r##"{
