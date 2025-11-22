921ad6e208ab9ec9bc6379577e0fb4c9c61d2913
Thomas <thomas.fractalbits@gmail.com>
2025-11-22 06:28:06 -0800

    fix(protoc): try to use system protobuf compiler if already installed
    Use the system protobuf compiler if already installed.

--- crates/common/rpc_codecs/rss_codec/build.rs
+++ crates/common/rpc_codecs/rss_codec/build.rs
@@ -1,7 +1,17 @@
-fn main() {
-    prost_build::Config::new()
-        .protoc_executable("../../../../third_party/protoc/bin/protoc")
+use cmd_lib::*;
+
+const THIRD_PARTY_PROTOC: &str = "../../../../third_party/protoc/bin/protoc";
+
+#[cmd_lib::main]
+fn main() -> CmdResult {
+    let mut prost_build = &mut prost_build::Config::new();
+    if run_cmd!(bash -c "command -v protoc").is_err() {
+        if std::path::Path::new(THIRD_PARTY_PROTOC).exists() {
+            prost_build = prost_build.protoc_executable(THIRD_PARTY_PROTOC);
+        }
+    }
+
+    prost_build
         .bytes(["."])
         .compile_protos(&["src/proto/rss_ops.proto"], &["src/proto/"])
-        .unwrap();
 }
